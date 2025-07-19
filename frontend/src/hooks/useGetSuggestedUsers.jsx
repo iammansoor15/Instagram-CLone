2ee9bc06586d37f760136eaa7b574b5005721830
Mansoor Patan <eesamansoor15000@gmail.com>
2025-07-19 10:08:39 +0530

    first commit

---
+++ frontend/src/hooks/useGetSuggestedUsers.jsx
@@ -0,0 +1,25 @@
+import { setSuggestedUsers } from "@/redux/authSlice";
+import axios from "axios";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+
+
+const useGetSuggestedUsers = () => {
+    const dispatch = useDispatch();
+    const BASE_URL = import.meta.env.VITE_BASE_URL;
+
+    useEffect(() => {
+        const fetchSuggestedUsers = async () => {
+            try {
+                const res = await axios.get(`${BASE_URL}/api/v1/user/suggested`, { withCredentials: true });
+                if (res.data.success) {
+                    dispatch(setSuggestedUsers(res.data.users));
+                }
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        fetchSuggestedUsers();
+    }, []);
+};
+export default useGetSuggestedUsers;
